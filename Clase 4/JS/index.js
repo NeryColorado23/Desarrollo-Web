8e769398090cd8ff8ed4eea24657330c81ac8517
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-08-02 10:58:39 -0600

    js

--- Clase 4/JS/index.js
+++ Clase 4/JS/index.js
@@ -1,11 +1,21 @@
 //variable declarar usar let que es variable global
 //var variable = "contenido"
-let variable = "contendio variable"
-const seccion = " - B"
+let variable = "contendio variable";
+const seccion = " - B";
 
 //mensaje de texto en consola javascript
-console.log("hola como estas", variable, seccion)
+console.log("hola como estas", variable, seccion);
 
-//estoy haciendo referencia al html aqui en js
-let texto = document.getElementById("contenido")
-console.log(texto)
+
+for(let i=0; etiqueta.length; i++){
+    etiqueta[i].style.background="red"
+}
+
+//estoy haciendo accediendo al html aqui en js desde su identificador
+let texto = document.getElementById("contenido");
+console.log(texto);
+
+//selector por etiqueta
+let etiqueta = document.getElementsByTagName("div");
+console.log(etiqueta);
+
