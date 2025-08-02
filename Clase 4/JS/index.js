d373d8205c4ada1cb8d01549962c803cb17178a7
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-08-02 10:43:05 -0600

    variables y constantes javascrip

--- Clase 4/JS/index.js
+++ Clase 4/JS/index.js
@@ -1,1 +1,7 @@
-console.log("hola como estas")
+//variable declarar usar let que es variable global
+//var variable = "contenido"
+let variable = "contendio variable"
+const seccion = " - B"
+
+//mensaje de texto en consola javascript
+console.log("hola como estas", variable, seccion)
