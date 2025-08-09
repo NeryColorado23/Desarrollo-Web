a7d91309b893910b48f9be98ba89a481d7463325
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-08-09 09:29:24 -0600

    clase 5

---
+++ Clase 5/JS/index.js
@@ -0,0 +1,8 @@
+let parrafo = document.getElementById("parrafo");
+console.log(parrafo)
+
+parrafo.style.color="blue";
+console.log(parrafo)
+
+let parrafos = document.getElementsByTagName("p")
+console.log(parrafos)
