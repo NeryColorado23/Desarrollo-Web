60ae982a146ff247c1bf176bd43648888ca941de
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-08-15 18:00:41 -0600

    ss

--- Hoja de Trabajo 4/JS/index.js
+++ Hoja de Trabajo 4/JS/index.js
@@ -317,7 +317,7 @@ function showLoadingProgress(container, message = 'Cargando...') {
 
 
 let konamiCode = [];
-const konamiSequence = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]; // ↑↑↓↓←→←→BA
+const konamiSequence = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]; 
 
 document.addEventListener('keydown', function(event) {
     konamiCode.push(event.keyCode);
