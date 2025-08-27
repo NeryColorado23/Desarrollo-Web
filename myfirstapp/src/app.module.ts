5a4414798555b310b39b2983eb0811a28b6257dc
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-08-27 07:20:26 -0600

    nuevo modulo de proyecto

--- myfirstapp/src/app.module.ts
+++ myfirstapp/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TaskModule } from './tasks/tasks.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [],
+  imports: [TaskModule, ProjectsModule],
 })
 export class AppModule {}
