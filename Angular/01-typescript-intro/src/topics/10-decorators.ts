2922c38b18fe1ea0179070aaca65de9d245be0d5
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-09-04 21:12:03 -0600

    cambios archivo 10

--- Angular/01-typescript-intro/src/topics/10-decorators.ts
+++ Angular/01-typescript-intro/src/topics/10-decorators.ts
@@ -1,7 +1,14 @@
 //una funcion no es mas que un simple decorador 
+function classDecorador <T extends {new (...args:any[]): {}}>(constructor:T){
+    return class extends constructor{
+        newProperty = 'New Property';
+        hello = 'override';
+    }
+}
+
+@classDecorador
 
 export class SuperClass {
-
     public myProperty: string = 'Abc123';
     print(){
         console.log('Hola Mundo')
