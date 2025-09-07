9faa5177234135ae490c6c5ac21520d716e17043
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-09-06 20:24:31 -0600

    cambios archivos

--- 02-bases/src/app/pages/dragonball/dragonball-page.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 interface Character{
   id: number;
@@ -7,13 +7,31 @@ interface Character{
 }
 
 @Component({
-  templateUrl: './dragonball-page.html'
+  templateUrl: './dragonball-page.html',
 })
 
+//creacion de señal
+
 export class DragonballPageComponent{
+
+  name = signal('Gohan');
+  power = signal(100);
+
+
   characters = signal<Character[]>([
     {id: 1, name: 'Goku', power: 9001},
     {id: 2, name: 'Vegeta', power: 8000},
-    {id: 1, name: 'Picolo', power: 3000}
-  ])
+    {id: 3, name: 'Picolo', power: 3000},
+    {id: 4, name: 'Yamcha', power: 500},
+  ]);
+
+  addCharacter() {
+    console.log(this.name(), this.power())
+}
+
+  powerClasses = computed(() => {
+    return {
+      'text-danger': true,
+    }
+  })
 }
