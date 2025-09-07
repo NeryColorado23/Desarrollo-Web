3c6609c2cd9e4212f9ec4d485bd469f2f412128a
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-09-06 20:36:56 -0600

    cambios

--- 02-bases/src/app/pages/dragonball/dragonball-page.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.ts
@@ -26,7 +26,23 @@ export class DragonballPageComponent{
   ]);
 
   addCharacter() {
-    console.log(this.name(), this.power())
+    if(!this.name()|| !this.power() || this.power() <= 0){
+      return;
+    }
+    const newCharacter: Character={
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    }
+    this.characters.update(
+      (list) => [...list, newCharacter]
+    );
+    this.resetFields();
+}
+
+resetFields(){
+  this.name.set('');
+  this.power.set(0);
 }
 
   powerClasses = computed(() => {
