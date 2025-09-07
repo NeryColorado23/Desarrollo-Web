f92077160311cdc6eb26fb4d2d950c58b0cfc46e
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-09-07 13:19:15 -0600

    servicios

--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.ts
@@ -1,12 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CharacterListComponent } from "../../components/dragonball/character-list/character-list";
 import { CharacteAddComponent } from "../../components/dragonball/character-add/character-add";
+import { DragonballService } from '../../services/dragonball.service';
 
-interface Character{
-  id: number;
-  name: string;
-  power: number;
-}
+
 
 @Component({
   templateUrl: './dragonball-super-page.html',
@@ -17,27 +14,8 @@ interface Character{
 //creacion de señal
 
 export class DragonballSuperPageComponent{
-
-  name = signal('');
-  power = signal(0);
-
-
-  characters = signal<Character[]>([
-    {id: 1, name: 'Goku', power: 9001},
-    {id: 2, name: 'Vegeta', power: 8000},
-  ]);
-
-  addCharacter(character: Character) {
-    this.characters.update(
-      list => [...list, character]
-    )
-
-}
-
-resetFields(){
-  this.name.set('');
-  this.power.set(0);
-}
+  //inyectar servicion dragonball
+  public dragonballServie = inject(DragonballService)
 
 
 }
