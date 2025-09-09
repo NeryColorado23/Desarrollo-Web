43f2ccf80d053ac87d98313a0e239e54f6aca996
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-09-09 10:07:02 -0600

    colores

--- sistema-medico/src/app/app.ts
+++ sistema-medico/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterPageComponent } from "./components/pages/footer-page/footer-page.component";
+import { HeaderPageComponent } from "./components/pages/header-page/header-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FooterPageComponent, HeaderPageComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ sistema-medico/src/app/components/pages/header-page/header-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-page',
+  imports: [],
+  templateUrl: './header-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderPageComponent { }
