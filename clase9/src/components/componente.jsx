a1e36134d273fcce704ba92df06df3c411cbcab4
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-09-13 09:44:05 -0600

    funcionde componentes en react

--- clase9/src/components/componente.jsx
+++ clase9/src/components/componente.jsx
@@ -1,7 +1,21 @@
-export default function Componente(){
-    let nombre = "Alan"
-    return <>
-    <h1>Estudiante {{nombre}}</h1>
-    <p>contenido</p>
-    </>
+import React, {useState} from "react"
+
+function Componente(){
+    const [nombre, setNombre] = useState("Alan")
+
+    function cambionombre(nuevoNombre){
+        setNombre(nuevoNombre)
+        console.log("Cambio de nombre", nuevoNombre)
+    }
+    
+    return (
+        <>
+            <h1>Estudiante {nombre}</h1>
+            <p>contenido</p>
+            <button onClick={() => cambionombre("Henry")}>Cambio de nombre</button>
+            <button onClick={() => cambionombre("Keyly")}>Cambio de nombre</button>
+        </>
+    )
 }
+
+export default Componente
