436b02191580a6f007e84bd7bb9704ebb0c8a260
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-09-13 10:47:13 -0600

    cambio

--- 03-gifts-app/src/app/gifs/pages/header-nav/header-nav.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
-@Component({
-  selector: 'app-header-nav',
-  imports: [RouterLink],
-  templateUrl: './header-nav.component.html',
-})
-export class HeaderNavComponent { }
