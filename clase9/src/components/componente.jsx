e46d99fefef051cce041bc4f5521e3be59e32f52
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-09-13 10:15:53 -0600

    user re

--- clase9/src/components/componente.jsx
+++ clase9/src/components/componente.jsx
@@ -1,12 +1,14 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, useRef} from "react"
 
 function Componente(){
     const [nombre, setNombre] = useState("Alan")
+    const referencia = useRef("Hola que hace");
 
     //ayuda a indicar si algo cambio en este hook uso de useEffect para controlar el cambio de estados de componentes
+    // usando [] se activara cuando se inicialize el componente
     useEffect(() => {
         console.log('Algo cambio')
-    })
+    }, [nombre])
 
     function cambionombre(nuevoNombre){
         setNombre(nuevoNombre)
@@ -16,7 +18,13 @@ function Componente(){
     return (
         <>
             <h1>Estudiante {nombre}</h1>
-            <p>contenido</p>
+            <p>{referencia.current.value}</p>
+            <input type="text" ref={referencia}></input>
+
+            <button onClick={()=> {console.log(referencia.current.value)}}>Mostar Valor</button>
+
+
+          
             <button onClick={() => cambionombre("Henry")}>Cambio de nombre</button>
             <button onClick={() => cambionombre("Keyly")}>Cambio de nombre</button>
         </>
