fe7fffee4330167361c42c3f5d1f810c0fb6a4f0
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-09-25 07:10:18 -0600

    piepes de tiempo y fechas

--- 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,15 +1,28 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
   standalone: true,
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class BasicPageComponent {
   namelower = signal('nery')
   nameUpper = signal('COLORADO')
-  fullname = signal('NEry ColoRADO')
+  fullname = signal('NEry ColoRADO');
+
+  customDate = signal(new Date());
+
+  tckingDateEffect = effect((onCleanUp) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date())
+      console.log('tick')
+    }, 1000);
+
+    onCleanUp((() => {
+      clearInterval(interval)
+    }))
+  })
 }
--- 05-pipes-app/src/app/pages/number-page/number-page.component.ts
+++ 05-pipes-app/src/app/pages/number-page/number-page.component.ts
@@ -1,9 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-number-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './number-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class NumberPageComponent { }
+export default class NumberPageComponent {
+  totalSells = signal(2_354_343.6785);
+  percent = signal(0.4856)
+ }
