857d501ca59f6037fbd014d1342731dad8cfdfcc
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-10-01 07:48:00 -0600

    mostrar errores en pantalla

--- 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -13,8 +13,8 @@ export class BasicPageComponent {
 
   //formbuilder es un servicio de reactiveformsmodule
   private fb = inject(FormBuilder);
-  myForm = this.fb.group({
-    //usar validaciones entre []
+  myForm: FormGroup = this.fb.group({
+    //usar validaciones de campos usando []
     name: ['', [Validators.required, Validators.minLength(3)]], //largo >=3
     price: [0, [Validators.required, Validators.min(10)]], //precio minimo de 10
     inStorage: [0, [Validators.required, Validators.min(0)]],
@@ -25,5 +25,32 @@ export class BasicPageComponent {
   //   price: new FormControl(0),
   //   inStorage: new FormControl(0),
   // })
+
+  //validar campos requeridos
+  isValidField(fieldName: string):boolean | null {
+    return !! this.myForm.controls[fieldName].errors
+  }
+
+  //validar largo minimo establecido para mostrar error en html
+  getFieldError(fieldName: string): string | null {
+    if(!this.myForm.controls[fieldName]) return null;
+
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+
+    for(const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+        return 'Este campo es requerido';
+
+        case 'minlength':
+        return `Minimo de ${errors['minlength'].requiredlength} caracteres.`;
+
+        case 'min':
+        return `Valor minimo de ${errors['min'].min}`;
+      }
+    }
+    return null;
+
+  }
 
 }
