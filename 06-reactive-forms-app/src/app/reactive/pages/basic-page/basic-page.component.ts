19e4d9c6c7f8d9a8347e5b403746508b57afe1c5
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-10-01 07:26:55 -0600

    errores validaciones

--- 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,7 +1,7 @@
 import { JsonPipe } from '@angular/common';
 import { JsonpInterceptor } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -10,10 +10,20 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BasicPageComponent {
-  myForm = new FormGroup({
-    name: new FormControl(''),
-    price: new FormControl(0),
-    inStorage: new FormControl(0),
+
+  //formbuilder es un servicio de reactiveformsmodule
+  private fb = inject(FormBuilder);
+  myForm = this.fb.group({
+    //usar validaciones entre []
+    name: ['', [Validators.required, Validators.minLength(3)]], //largo >=3
+    price: [0, [Validators.required, Validators.min(10)]], //precio minimo de 10
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   })
+
+  // myForm2 = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // })
 
 }
