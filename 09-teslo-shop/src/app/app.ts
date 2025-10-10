e6a4c7306f54d51b7026cd6a6e406c3f9681665f
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-10-10 15:28:48 -0600

    cabmios

--- 09-teslo-shop/src/app/app.ts
+++ 09-teslo-shop/src/app/app.ts
@@ -5,7 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   //imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [RouterOutlet]
 })
 export class App {
   protected readonly title = signal('teslo-shop');
