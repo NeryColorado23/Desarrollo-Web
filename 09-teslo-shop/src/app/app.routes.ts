38a031fe5babfda75d81aad5e2bc94bd92b675b8
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-10-10 18:24:36 -0600

    formato de texto

--- 09-teslo-shop/src/app/app.routes.ts
+++ 09-teslo-shop/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    //llame ruta de mi store front que tiene su propio archivo de ruta
+    path: '',
+    loadChildren: () => import('./store-front/store-front.routes')
+  }
+];
