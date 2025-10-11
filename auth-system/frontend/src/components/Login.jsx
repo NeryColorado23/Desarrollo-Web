276875a60206f2e79518912e85d2b0d9cdcaf3f5
NeryColorado23 <ncoloradom@miumg.edu.gt>
2025-10-11 09:08:17 -0600

    cambios

--- auth-system/frontend/src/components/Login.jsx
+++ auth-system/frontend/src/components/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:3001/login', {
+      const response = await fetch('https://desarrollo-web-1ahf.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- auth-system/frontend/src/components/Register.jsx
+++ auth-system/frontend/src/components/Register.jsx
@@ -27,7 +27,7 @@ const Register = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:3001/register', {
+      const response = await fetch('https://desarrollo-web-1ahf.onrender.com/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
